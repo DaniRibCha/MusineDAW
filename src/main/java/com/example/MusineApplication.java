2b6534c92b381023e1cab3621ea56b12a24250b7
Frego22 <fregonesedavide@gmail.com>
2017-02-27 12:34:51 +0100

    Añadido a carpeta Spring

---
+++ src/main/java/com/example/MusineApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusineApplication.class, args);
+	}
+}
