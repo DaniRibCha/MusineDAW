6e65fdd32ca327111c000e053ae00191e5f923ea
Frego22 <fregonesedavide@gmail.com>
2017-03-13 14:48:33 +0100

    correccion de gestion de la sesion
    control de la sesion en la pagina principal (index)

--- spring/src/main/java/com/example/UserController.java
+++ spring/src/main/java/com/example/UserController.java
@@ -17,9 +17,45 @@ public class UserController {
 	@Autowired
 	private UserComponent userComponent;
 	
-	@RequestMapping("login")
+	@RequestMapping("/login")
 	public String getLogin(){
 		return "login";
+	}
+	
+	@RequestMapping("loginDone")
+	public String getLoginIndex(HttpSession session, Model model,
+			@RequestParam(value = "userName", defaultValue = "") String userName,
+			@RequestParam(value = "email", defaultValue = "") String email, 
+			@RequestParam(value = "password", defaultValue = "") String password){
+		
+		boolean login=false;
+		
+		User u;
+		//mail vacia->form del login
+		if(email.equals("")){
+			u=userRepository.findByName(userName);
+			if(u==null){ //nombre no correcto
+				session.invalidate();
+				return "login";
+			}
+			String psw=u.getPasswordHash();
+			if(password.equals(psw)){
+				login=true;
+				u.addRole("USER");
+				userRepository.save(u);
+				//session.setMaxInactiveInterval(30);
+				session.setAttribute("idUser", u.getId_user());
+			}else{//nombre correcto y password no correcta
+				session.invalidate();
+				return "login";
+			}
+		}
+		
+		model.addAttribute("idUser",session.getAttribute("idUser"));
+		
+		model.addAttribute("login",login);
+		
+		return "index";
 	}
 	
 	
@@ -36,44 +72,15 @@ public class UserController {
 	}
 	
 	@RequestMapping("/")
-	public String getIndex(HttpSession session, Model model,
-			@RequestParam(value = "userName", defaultValue = "") String userName,
-			@RequestParam(value = "email", defaultValue = "") String email, 
-			@RequestParam(value = "password", defaultValue = "") String password){
+	public String getIndex(HttpSession session, Model model){
 		
 		boolean login=false;
 		
-		if(session.isNew()){
-			User u;
-			//mail vacia->form del login
-			if(email.equals("")){
-				u=userRepository.findByName(userName);
-				if(u==null){ //nombre no correcto
-					session.invalidate();
-					return "login";
-				}
-				String psw=u.getPasswordHash();
-				if(password.equals(psw)){
-					login=true;
-					//session.setMaxInactiveInterval(30);
-					session.setAttribute("idUser", u.getId_user());
-				}else{//nombre correcto y password no correcta
-					session.invalidate();
-					return "login";
-				}
-			}else{//form del registro de nuevo usuario
-				u=new User(userName, "unknown", password, email);
-				userRepository.save(u);
-				u=userRepository.findByName(userName);
-				login=true;
-				session.setAttribute("idUser", u.getId_user());
-				//session.setMaxInactiveInterval(30);
-			}
-		}else{
+		if(!session.isNew()){
 			login=true;
+			model.addAttribute("idUser",session.getAttribute("idUser"));
 		}
-		
-		model.addAttribute("idUser",session.getAttribute("idUser"));
+			
 		
 		model.addAttribute("login",login);
 		
