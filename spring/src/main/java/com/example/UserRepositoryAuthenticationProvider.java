3a709ccf2643a1f3b2f5e3daf184a9798e06d6f7
RubenIgle <chubioficial@gmail.com>
2017-03-14 16:01:05 +0100

    Remplazo por error

--- spring/src/main/java/com/example/UserRepositoryAuthenticationProvider.java
+++ spring/src/main/java/com/example/UserRepositoryAuthenticationProvider.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +38,7 @@ public class UserRepositoryAuthenticationProvider implements AuthenticationProvi
 			throw new BadCredentialsException("Wrong password");
 		}
 		
-		//userComponent.setId();
+		//userComponent.setLoggedUser(user);
 
 		List<GrantedAuthority> roles = new ArrayList<>();
 		for (String role : user.getRoles()) {
@@ -53,60 +52,4 @@ public class UserRepositoryAuthenticationProvider implements AuthenticationProvi
 	public boolean supports(Class<?> authenticationObject) {
 		return true;
 	}
-=======
-package com.example;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Component;
-@Component
-public class UserRepositoryAuthenticationProvider implements AuthenticationProvider {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private UserComponent userComponent;
-
-	@Override
-	public Authentication authenticate(Authentication auth) throws AuthenticationException {
-		
-		String username = auth.getName();
-		String password = (String) auth.getCredentials();
-		
-		User user = userRepository.findByName(username);
-		
-		if(user == null){
-			throw new BadCredentialsException("User not found");
-		}
-		
-		
-		if (!new BCryptPasswordEncoder().matches(password, user.getPasswordHash())) {
-			throw new BadCredentialsException("Wrong password");
-		}
-		
-		//userComponent.setId();
-
-		List<GrantedAuthority> roles = new ArrayList<>();
-		for (String role : user.getRoles()) {
-			roles.add(new SimpleGrantedAuthority(role));
-		}
-
-		return new UsernamePasswordAuthenticationToken(user.getName(), password, roles);
-	}
-	
-	@Override
-	public boolean supports(Class<?> authenticationObject) {
-		return true;
-	}
->>>>>>> ea66b2c88781e908b14facfb9dca4431aad916f2
 }
