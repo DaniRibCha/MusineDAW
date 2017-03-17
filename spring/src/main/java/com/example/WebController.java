fc28bbc669c039d2eff1c99acd46cf31b85ed97f
Frego22 <fregonesedavide@gmail.com>
2017-03-17 12:47:10 +0100

    posibilidad registrarse desde login

--- spring/src/main/java/com/example/WebController.java
+++ spring/src/main/java/com/example/WebController.java
@@ -41,22 +41,113 @@ public class WebController {
     public String loginerror() {
     	return "loginerror";
     }
-
-    @RequestMapping("/home")
-    public String home(Model model, HttpServletRequest request) {
+    
+    @RequestMapping("signIn")
+    public String signIn(Model model,
+    		@RequestParam(value = "username") String username,
+    		@RequestParam(value = "email") String email,
+    		@RequestParam(value = "password") String password){
+    	User u=new User(username, "",password,email,"ROLE_USER");
+    	userRepository.save(u);
+    	userComponent.setLoggedUser(u);
+boolean login=userComponent.isLoggedUser();
     	
-    	model.addAttribute("admin", request.isUserInRole("ADMIN"));
     	
-    	Principal p = request.getUserPrincipal();
-    	User user = userRepository.findByName(p.getName());
     	
-    	User user2 = userComponent.getLoggedUser();
+//    	Principal principal = request.getUserPrincipal();
+//    	User user = userRepository.findByName(principal.getName());
     	
-    	System.out.println("User: "+user);
-    	System.out.println("User2: "+user2);
+    	//User u=userComponent.getLoggedUser();
     	
-    	return "home";
+    	
+		List<Playlist> topPlaylists=new ArrayList<>();
+		
+		topPlaylists=playlistRepository.findFirst3ByOrderByNLikesDesc();
+		
+		model.addAttribute("topPlaylists",topPlaylists);
+		
+		List<Artist> topArtists=new ArrayList<>();
+		
+		topArtists=artistRepository.findFirst3ByOrderByFollowersDesc();
+		
+		model.addAttribute("topArtists",topArtists);
+		
+		List<Tag> topTags=new ArrayList<>();
+		
+		topTags=tagRepository.findFirst3ByOrderByNumberTagDesc();
+		
+		model.addAttribute("topTags",topTags);
+		
+		List<Playlist> wallPlaylists=new ArrayList<>();
+//		
+		if(!login){
+			wallPlaylists=playlistRepository.findFirst3ByOrderByNLikesDesc();
+		}else{
+			User uLogged=userRepository.findOne(userComponent.getIdLoggedUser());
+			List<Playlist> recentPlaylists=playlistRepository.findFirst100ByOrderByDateAsc();
+			List<User> followingByLogged=uLogged.getFollowing();
+			boolean betweenFollowed;
+			for(int i=0;i<recentPlaylists.size();++i){
+				Playlist p=recentPlaylists.get(i);
+				betweenFollowed=false;
+				for(int j=0;j<followingByLogged.size() && !betweenFollowed;++j){
+					if(p.getCreatorId()==followingByLogged.get(j).getId_user()){
+						betweenFollowed=true;
+					}
+				}
+				if(!betweenFollowed) recentPlaylists.remove(p);
+			}
+			wallPlaylists=recentPlaylists;
+		}
+		
+		//cheque el atributo booelan idLogged
+		//para trazar la ruta dinamica de cada playlist desde la plantilla
+		if(login){
+			User uLogged=userRepository.findOne(userComponent.getIdLoggedUser());
+			long idLogged=uLogged.getId_user();
+			
+			for(int i=0;i<wallPlaylists.size();++i){
+				Playlist p=wallPlaylists.get(i);
+				if(p.getCreatorId()==idLogged){
+					p.setIdLogged(true);
+				}
+			}
+		}
+		
+		model.addAttribute("wallPlaylists",wallPlaylists);
+		
+		if(login){
+			User uLogged=userRepository.findOne(userComponent.getIdLoggedUser());
+			List<Artist> artistsFollowedByLogged=uLogged.getFollowingArtists();
+			model.addAttribute("artistsFollowedByLogged",artistsFollowedByLogged);
+		}
+		
+		if(login){
+			User uLogged=userRepository.findOne(userComponent.getIdLoggedUser());
+			model.addAttribute("uLogged",uLogged);
+		}
+		
+		model.addAttribute("login", login);
+		
+		return "index";
     }
+    
+//
+//    @RequestMapping("/home")
+//    public String home(Model model, HttpServletRequest request) {
+//    	
+//    	model.addAttribute("admin", request.isUserInRole("ADMIN"));
+//    	
+//    	Principal p = request.getUserPrincipal();
+//    	User user = userRepository.findByName(p.getName());
+//    	
+//    	User user2 = userComponent.getLoggedUser();
+//    	
+//    	System.out.println("User: "+user);
+//    	System.out.println("User2: "+user2);
+//    	
+//    	return "home";
+//    }
     
     @RequestMapping("/")
 	public String getIndex(HttpSession session, Model model, HttpServletRequest request,
