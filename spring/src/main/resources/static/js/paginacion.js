b4cc5839cc115cf41b3614328f4a7cf063066716
Frego22 <fregonesedavide@gmail.com>
2017-03-20 20:52:39 +0100

    avanzando paginacion

--- spring/src/main/resources/static/js/paginacion.js
+++ spring/src/main/resources/static/js/paginacion.js
@@ -4,23 +4,17 @@ $(document).ready(function () {
 	$("#loadMore").click(function () {
 		
 		$('#spin').html("<img src='../img/loader.gif'/>");
-	        
 	        $.ajax({
-	            url: "/UserFollowing/4?page=1"
+	            url: "/UserFollowing/"+user+"?page="+nPage
+	        	//url: "/UserFollowing/5?page=1"
 	        }).done(function (data) {
-	            
+	        	 
+	        	
 	            $('#spin').empty()
+	            $('#spin').append("<div><h3>ciao</h3></div>");
 	            
-	            $('#more').append(data);
-	            
-	            console.log(JSON.stringify(data));
 
-	            //for (var i = 0; i < data.length; i++) {
-
-	              //  var seguidores = data[i];
-
-	                //$('#more').append("<div><h3>"+ seguidores +"</p></div>");
-	            //}
+	           
 	        });
 	        
 	})
