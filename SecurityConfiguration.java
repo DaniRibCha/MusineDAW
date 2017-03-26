1581167bf75cc24dbc99cca56c6acfc038480af5
DaniRibCha <dani_titihenry@hotmail.com>
2017-03-26 19:32:56 +0200

    Limpieza carpeta src

--- SecurityConfiguration.java
+++ SecurityConfiguration.java
@@ -1,77 +1,75 @@
 package com.example;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	UserRepositoryAuthenticationProvider authenticationProvider;
-	
+ @Override
+protected void configure(AuthenticationManagerBuilder auth)
+throws Exception {
+
+ // Users
+ auth.inMemoryAuthentication().withUser("user").password("pass")
+ .roles("USER");
+
+auth.inMemoryAuthentication().withUser("admin").password("adminpass").roles("USER", "ADMIN");
+}
+ @Override
+protected void configure(HttpSecurity http) throws Exception {
+
+ // Public pages
+	 http.authorizeRequests().antMatchers("/").permitAll();
+ // Private pages (all other pages)
+ http.authorizeRequests().antMatchers("/Artist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/createPlaylist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/editPlaylist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/facebook").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/google").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/UserPlaylists/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/ArtistSong/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/PlaylistSongs/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/SongListBasic").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/PlaylistTags/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/UserFollowing/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/UserFollowers/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/UserFavorites/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/UserLikes/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/MyFollowers/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/MyPlaylists/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/MyLikes/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/MyFavorites/{id}}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/ArtistFollowers/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/Artist/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/Playlist/{id}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/SearchPlaylist/{key}").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/ArtistListBasic").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/homestyle").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/index").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/indexUtent_favoritos").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/indexUtent_likes").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/indexUtent_playlist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/login").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/Playlist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/searchPlaylist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/searchUtent_playlist").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/searchUtent_likes").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("//searchUtent_tracks").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("//seguidores_otroUsuario").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("//seguidores").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("//seguidos_otroUsuario").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("//seguidos").hasAnyRole("USER");
+ http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
+
  
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.headers().frameOptions().disable();
-		
-		// Public pages
-		http.authorizeRequests().antMatchers("/").permitAll();
-		http.authorizeRequests().antMatchers("/login").permitAll();
-		http.authorizeRequests().antMatchers("/Artist/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/Playlist/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/ArtistFollowers/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/ArtistSong/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/SearchPlaylist/{key}").permitAll();
-		http.authorizeRequests().antMatchers("/ArtistListBasic").permitAll();
-		http.authorizeRequests().antMatchers("/SearchPlaylist").permitAll();
-		http.authorizeRequests().antMatchers("/UserPlaylists/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/SongListBasic").permitAll(); 
-		http.authorizeRequests().antMatchers("/UserFollowing/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/UserFollowers/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/UserFavorites/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/UserLikes/{id}").permitAll();
-		http.authorizeRequests().antMatchers("/UserPlaylists/{id}").permitAll();
-		
-		// Private pages (all other pages)
+ 
+ 
+ 
+ // Login form
 
-		http.authorizeRequests().antMatchers("/CreatePlaylist").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/EditPlaylist/{idPlaylist}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/EditNewPlaylist/{idUser}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/MyFollowers/{id}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/MyFollowing/{id}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/MyPlaylists/{id}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/MyLikes/{id}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/MyFavorites/{id}").hasAnyRole("USER");
-		http.authorizeRequests().antMatchers("/Config/{id}").hasAnyRole("USER"); 
-		http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
-		http.authorizeRequests().antMatchers("/adminCreateArtist").hasAnyRole("ADMIN");
-		http.authorizeRequests().antMatchers("/adminEditArtist").hasAnyRole("ADMIN");
-		http.authorizeRequests().antMatchers("/adminCreateSong").hasAnyRole("ADMIN");
-		http.authorizeRequests().antMatchers("/adminEditSong").hasAnyRole("ADMIN");
+ // Logout
 
-		// Login form
-		 http.formLogin().loginPage("/login");
-	        http.formLogin().usernameParameter("username");
-	        http.formLogin().passwordParameter("password");
-	        http.formLogin().defaultSuccessUrl("/");
-	        http.formLogin().failureUrl("/loginerror");
-		// Logout
-	        http.logout().logoutUrl("/logout");
-	        http.logout().logoutSuccessUrl("/");
-		//disable csfr
-		//http.csrf().disable();
-
-	}
-	
-	  @Override
-	    protected void configure(AuthenticationManagerBuilder auth)
-	            throws Exception {
-	        // Database authentication provider
-	        auth.authenticationProvider(authenticationProvider);
-	    }
+ }
 
 }
