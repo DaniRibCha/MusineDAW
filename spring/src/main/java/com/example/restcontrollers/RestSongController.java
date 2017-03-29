8ce13db2b5f3542ad3a0e2df7eada618e96fea88
RubenIgle <chubioficial@gmail.com>
2017-03-29 21:21:49 +0200

    SongsOfArtinst en RestSongController

--- spring/src/main/java/com/example/restcontrollers/RestSongController.java
+++ spring/src/main/java/com/example/restcontrollers/RestSongController.java
@@ -1,5 +1,7 @@
 package com.example.restcontrollers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.classes.Artist;
+import com.example.classes.Playlist;
 import com.example.classes.Song;
 import com.example.classes.Tag;
 import com.example.classes.User;
 import com.example.restcontrollers.RestArtistController.ArtistFollowersView;
 import com.example.restcontrollers.RestArtistController.ArtistView;
+import com.example.restcontrollers.RestPlaylistController.MyPlaylistsView;
 import com.example.services.ArtistService;
 import com.example.services.SongService;
 import com.fasterxml.jackson.annotation.JsonView;
@@ -22,7 +26,7 @@ public class RestSongController {
 	@Autowired
 	private SongService songService;
 	
-	
+	private ArtistService artistService;
 	
 	interface SongView extends Song.Basic, Song.Artists,Song.Playlists{};
 	
@@ -33,5 +37,16 @@ public class RestSongController {
 		return new ResponseEntity<>(s,HttpStatus.OK);
 	}
 	
-
+interface SongsOfArtistView extends Artist.Songs, Song.Basic,Song.Artists,Song.Playlists{}
+	
+	@JsonView(SongsOfArtistView.class)
+	@RequestMapping("/api/SongsOfArtist/{id}")
+	public ResponseEntity<List<Song>> getSongsOfArist(@PathVariable long id) throws Exception{
+		Artist a= artistService.findOne(id);
+		List<Song> songs=a.getSongsOfArtist();
+		return new ResponseEntity<>(songs,HttpStatus.OK);
+	}
+	
 }
+	
+
