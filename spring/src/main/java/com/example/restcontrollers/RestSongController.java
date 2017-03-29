cf207799ee786d533895446873a9eb67c876c09c
RubenIgle <chubioficial@gmail.com>
2017-03-29 18:51:13 +0200

    RestSongController

---
+++ spring/src/main/java/com/example/restcontrollers/RestSongController.java
@@ -0,0 +1,37 @@
+package com.example.restcontrollers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.classes.Artist;
+import com.example.classes.Song;
+import com.example.classes.Tag;
+import com.example.classes.User;
+import com.example.restcontrollers.RestArtistController.ArtistFollowersView;
+import com.example.restcontrollers.RestArtistController.ArtistView;
+import com.example.services.ArtistService;
+import com.example.services.SongService;
+import com.fasterxml.jackson.annotation.JsonView;
+
+@RestController
+public class RestSongController {
+	@Autowired
+	private SongService songService;
+	
+	
+	
+	interface SongView extends Song.Basic, Song.Artists,Song.Playlists{};
+	
+	@JsonView(SongView.class)
+	@RequestMapping("/api/Song/{id}")
+	public ResponseEntity<Song> getArtistSongs(@PathVariable long id) throws Exception{
+		Song s=songService.findOne(id);
+		return new ResponseEntity<>(s,HttpStatus.OK);
+	}
+	
+
+}
