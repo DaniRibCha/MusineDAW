9ff5c4b0030c4d934d40116079d0e664593e3069
RubenIgle <chubioficial@gmail.com>
2017-03-31 18:27:05 +0200

    Add Favorite Song Delete Favorite Song

--- spring/src/main/java/com/example/restcontrollers/RestSongController.java
+++ spring/src/main/java/com/example/restcontrollers/RestSongController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.classes.Artist;
@@ -16,9 +17,12 @@ import com.example.classes.Tag;
 import com.example.classes.User;
 import com.example.restcontrollers.RestArtistController.ArtistFollowersView;
 import com.example.restcontrollers.RestArtistController.ArtistView;
+import com.example.restcontrollers.RestPlaylistController.DeletePlaylistView;
 import com.example.restcontrollers.RestPlaylistController.MyPlaylistsView;
+import com.example.security.UserComponent;
 import com.example.services.ArtistService;
 import com.example.services.SongService;
+import com.example.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
 
 @RestController
@@ -27,6 +31,11 @@ public class RestSongController {
 	private SongService songService;
 	@Autowired
 	private ArtistService artistService;
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private UserComponent userComponent;
+	
 	
 	interface SongView extends Song.Basic, Song.Artists,Song.Playlists{};
 	
@@ -37,6 +46,44 @@ public class RestSongController {
 		return new ResponseEntity<>(s,HttpStatus.OK);
 	}
 	
+	interface AddFavoriteSongView extends Song.Basic, Song.Playlists, Song.Artists{};
+		
+		@JsonView(AddFavoriteSongView.class)
+		@RequestMapping(value="/api/AddFavoriteSong/{id}", method=RequestMethod.POST)
+		public ResponseEntity<Song> AddFavoriteSong(@PathVariable long id){
+			Song favoriteSong=songService.findOne(id);
+			User u=userComponent.getLoggedUser();
+			List<Song> l=u.getFavoriteSongs();
+			l.add(favoriteSong);
+			songService.save(favoriteSong);	
+			userService.save(u);
+			if (favoriteSong != null) {
+				return new ResponseEntity<>(favoriteSong, HttpStatus.OK);
+				}
+				else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}
+	
+	
+	interface DeleteFavoriteSongView extends User.Favorites, Song.Basic, Song.Playlists, Song.Artists{};
+	
+	@JsonView(DeleteFavoriteSongView.class)
+	@RequestMapping(value="/api/DeleteFavoriteSong/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Song> DeleteFavoriteSong(@PathVariable long id){
+		Song favoriteSong=songService.findOne(id);
+		User u=userComponent.getLoggedUser();
+		List<Song> l=u.getFavoriteSongs();
+		l.remove(favoriteSong);
+		songService.save(favoriteSong);	
+		userService.save(u);
+		if (favoriteSong != null) {
+			return new ResponseEntity<>(favoriteSong, HttpStatus.OK);
+			}
+			else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
 	
 }
