f80df652c67c620d6129c146a60ce0c6d848130f
StephPoleo <stephaniepoleo@hotmail.com>
2017-04-05 22:59:16 +0200

    Actualizacion de la paginacion

--- spring/src/main/resources/static/js/paginacion.js
+++ spring/src/main/resources/static/js/paginacion.js
@@ -1,21 +1,14 @@
 
 $(document).ready(function () {
 	
+	var url = document.getElementById("url").value;
+	
 	$("#loadMore").click(function () {
 		
 		$('#spin').html("<img src='../img/loader.gif'/>");
-	        $.ajax({
-	            url: "/UserFollowing/5?page=1"
-	        	//url: "/UserFollowing/5?page=1"
-	        }).done(function (data) {
-	        	 
-	        	
+	        	         	
 	            $('#spin').empty()
-	            //$('#spin').append(data);
-	            $('#spin').load("/MyFollowing/5?page=1 #pag");
+	            $('#spin').load(url + "#pag");
 
-	           
-	        });
-	        
 	})
 })
